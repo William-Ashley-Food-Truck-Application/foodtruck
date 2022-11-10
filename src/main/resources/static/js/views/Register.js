f2728e1df45de897b3e5e68a7fec4e91faea7203
withers56 <william.k.withers@gmail.com>
2022-11-09 19:53:27 -0600

    refactored register page to not use username

--- src/main/resources/static/js/views/Register.js
+++ src/main/resources/static/js/views/Register.js
@@ -8,64 +8,25 @@ export default function Register(props) {
                 <meta charset="UTF-8"/>
                 <title>Register</title>
             </head>
-            <body>
-<!--                <h1>Register</h1>-->
-        
-<!--                <form id="register-form">-->
-<!--                    <label for="username">Username</label>-->
-<!--                    <input id="username" name="username" type="text"/>-->
-<!--                    <label for="email">Email</label>-->
-<!--                    <input id="email" name="email" type="email">-->
-<!--                    <label for="password">Password</label>-->
-<!--                    <input id="password" name="password" type="password"/>-->
-<!--                    <button id="register-btn" type="button">Register</button>-->
-<!--                </form>-->
-                
-                
-<!--                <div class="card w-50 m-auto">-->
-<!--                  <div class="card-body">-->
-<!--                    <h5 class="card-title">Register</h5>-->
-<!--                    -->
-<!--                    <form id="register-form">-->
-<!--                        <div><label for="username">Username</label>-->
-<!--                        <input id="username" name="username" type="text"/></div>-->
-<!--                        <div><label for="email">Email</label>-->
-<!--                        <input id="email" name="email" type="email"></div>-->
-<!--                        <div><label for="password">Password</label>-->
-<!--                        <input id="password" name="password" type="password"/></div>-->
-<!--                         <a href="#" id="register-btn" class="btn btn-primary">register</a>-->
-
-<!--                    </form>-->
-                    
-                    
-                    <div class="card w-50 m-auto register-card">
-                        <div class="card-header background-card-dark">
-                            <h2>Register</h2>
-                        </div>
-                      <div class="card-body background-card-dark">
-                        <form id="register-form">
-                      <div class="mb-3">
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control background-card-dark" id="username" aria-describedby="emailHelp">
-                        
-                      <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control background-card-dark" id="email" aria-describedby="emailHelp">
-                        
-                      <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control background-card-dark" id="password">
-                      </div>
-                      <button type="button" class="btn btn-primary" id="register-btn">Register</button>
-                    </form>
-                      </div>
+            <body>  
+                <div class="card w-50 m-auto register-card">
+                    <div class="card-header background-card-dark">
+                        <h2>Register</h2>
                     </div>
-                    
-                    
-                    
-                    
-                  
-                
+                    <div class="card-body background-card-dark">
+                        <form id="register-form">                    
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control background-card-dark" id="email" aria-describedby="emailHelp">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control background-card-dark" id="password">
+                            </div>
+                            <button type="button" class="btn btn-primary" id="register-btn">Register</button>
+                        </form>
+                    </div>
+                </div>      
             </body>
         </html>
 `;
@@ -74,12 +35,10 @@ export default function Register(props) {
 export function RegisterEvent(){
     $("#register-btn").click(function(){
 
-        const username = $("#username").val();
         const email = $("#email").val();
         const password = $("#password").val()
 
         let newUser = {
-            username,
             email,
             password
         }
